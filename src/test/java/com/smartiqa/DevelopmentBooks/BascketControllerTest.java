9e412b319e6288aa5474df219bc43b1340254f7f
chafik.gouasmia@axa.dz <chafik.gouasmia@axa.dz>
2022-06-29 22:40:48 +0100

    added test for response status code (fail)

---
+++ src/test/java/com/smartiqa/DevelopmentBooks/BascketControllerTest.java
@@ -0,0 +1,42 @@
+package com.smartiqa.DevelopmentBooks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class BascketControllerTest {
+
+  //Inject Support Utils
+  private static MockHttpServletRequest request;
+  @Autowired
+  MockMvc mockMvc;
+  @Autowired
+  ObjectMapper objectMapper;
+  public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
+
+  // Inject services
+
+
+  @BeforeAll
+  public static void setup() {
+    request = new MockHttpServletRequest();
+  }
+
+  @Test
+  public void checkPostBooksResponseStatusCode () throws Exception {
+    List<Book> bookList = new ArrayList<Book>();
+
+  }
+
+}
