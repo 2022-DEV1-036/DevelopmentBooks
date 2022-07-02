473b0ecb8b3d84d9e019b572d83c2f6945bfba74
chafik.gouasmia@axa.dz <chafik.gouasmia@axa.dz>
2022-07-02 22:54:48 +0100

    Added Test for 5 different Books discount calculation (RED)

--- src/test/java/com/smartiqa/DevelopmentBooks/services/BookDiscountServiceImplTest.java
+++ src/test/java/com/smartiqa/DevelopmentBooks/services/BookDiscountServiceImplTest.java
@@ -84,7 +84,9 @@ public class BookDiscountServiceImplTest {
 
     assertEquals(5, numberOfDifferentBooks);
   }
-  // test for applying  5 % discount for only two different Books in the basket
+  /* Test for applying  5 % discount for only two different Books in the basket
+  *  Rule = 2 different Books in the basket list of Books ==> apply  5 % discount on the TotalBasketPrice :)
+  * */
   @Test
   public void shouldApply5percentDiscountForTwoDifferentBooksInBasket() throws Exception {
     List<Book> bookList = new ArrayList<Book>();
@@ -111,7 +113,9 @@ public class BookDiscountServiceImplTest {
     assertEquals(new BasketDiscountPrice(427.5,9), basketDiscountPrice);
   }
 
-  // test for applying 10 % discount for only Three different Books in the basket
+  /* Test for applying  10 % discount for only Three different Books in the basket
+   *  Rule = 3 different Books in the basket list of Books ==> apply  10 % discount on the TotalBasketPrice :)
+   * */
   @Test
   public void shouldApply10percentDiscountForThreeDifferentBooksInBasket() throws Exception {
     List<Book> bookList = new ArrayList<Book>();
@@ -137,7 +141,10 @@ public class BookDiscountServiceImplTest {
 
     assertEquals(new BasketDiscountPrice(405.00,9), basketDiscountPrice);
   }
-  // test for applying 20 % discount for only four different Books in the basket
+
+  /* Test for applying  20 % discount for only Three different Books in the basket
+   *  Rule = 4 different Books in the basket list of Books ==> apply  20 % discount on the TotalBasketPrice :)
+   * */
   @Test
   public void shouldApply20percentDiscountForFourDifferentBooksInBasket() throws Exception {
     List<Book> bookList = new ArrayList<Book>();
@@ -164,6 +171,35 @@ public class BookDiscountServiceImplTest {
     assertEquals(new BasketDiscountPrice(360.00,9), basketDiscountPrice);
   }
 
+  /* Test for applying  25 % discount for only Three different Books in the basket
+   *  Rule = 5 different Books in the basket list of Books ==> apply  25 % discount on the TotalBasketPrice :)
+   * */
+
+  @Test
+  public void shouldApply25percentDiscountForFourDifferentBooksInBasket() throws Exception {
+    List<Book> bookList = new ArrayList<Book>();
+    Book book1 = new Book("Clean Code (Robert Martin, 2008)", 50.00);
+    Book book2 = new Book("The Clean Coder (Robert Martin, 2011)", 50.00);
+    Book book3 = new Book("Clean Architecture (Robert Martin, 2017)", 50.00);
+    Book book4 = new Book("Test Driven Development by Example (Kent Beck, 2003)", 50.00);
+    Book book5 = new Book("Working Effectively With Legacy Code (Michael C. Feathers, 2004)", 50.00);
+    Book book6 = new Book("The Clean Coder (Robert Martin, 2011)", 50.00);
+    Book book7 = new Book("Clean Architecture (Robert Martin, 2017)", 50.00);
+    Book book8 = new Book("Test Driven Development by Example (Kent Beck, 2003)", 50.00);
+    Book book9 = new Book("Working Effectively With Legacy Code (Michael C. Feathers, 2004)", 50.00);
+    bookList.add(book1);
+    bookList.add(book2);
+    bookList.add(book3);
+    bookList.add(book4);
+    bookList.add(book5);
+    bookList.add(book6);
+    bookList.add(book7);
+    bookList.add(book8);
+    bookList.add(book9);
+    BasketDiscountPrice basketDiscountPrice = bookDiscountServiceImpl.calculateDiscount(bookList);
+
+    assertEquals(new BasketDiscountPrice(337.5,9), basketDiscountPrice);
+  }
 
 
 }
